ee9e188b1f01672b93fd21a4a18ae51e4b5d7e5f
Aaron Boxer <aaron.boxer@collabora.com>
2024-05-29 12:23:37 -0400

    llama: add checkpoint download and conversion

--- torchbenchmark/models/llama/__init__.py
+++ torchbenchmark/models/llama/__init__.py
@@ -20,8 +20,8 @@ class Model(BenchmarkModel):
         self.model_args = ModelArgs(vocab_size=32000,device=self.device)
         torch.set_default_device(self.device)
         with torch.device('meta'):
-            self.model = Transformer.from_name("meta-llama/Llama-2-7b-chat-hf")
-        checkpoint_path = Path(r"..\pytorch-directml-plugin\samples\llm\checkpoints\meta-llama\Llama-2-7b-chat-hf\model.pth")
+            self.model = Transformer.from_name("meta-llama/Llama-2-7b-hf")
+        checkpoint_path = Path(r"torchbenchmark\models\llama\.data\checkpoints\meta-llama\Llama-2-7b-hf\model.pth")
         print(checkpoint_path)
         checkpoint = torch.load(str(checkpoint_path), mmap=True, weights_only=True)
         self.model.load_state_dict(checkpoint, assign=True)
